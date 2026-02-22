c5407ddc1a0200bd252caa6af8784f5a4862b2be
James S. Moore <james@webtechhq.com>
2026-02-21 23:10:54 -0600

    election day

--- election-day/election_day.go
+++ election-day/election_day.go
@@ -37,5 +37,7 @@ func DisplayResult(result *ElectionResult) string {
 
 // DecrementVotesOfCandidate decrements by one the vote count of a candidate in a map.
 func DecrementVotesOfCandidate(results map[string]int, candidate string) {
-	panic("Please implement the DecrementVotesOfCandidate() function")
+	if results[candidate] >= 1 {
+		results[candidate]--
+	}
 }

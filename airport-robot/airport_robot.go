35b182e401704c2a4cc2967ef6bfce9f3f4536e7
James S. Moore <james@webtechhq.com>
2026-02-23 12:14:07 -0600

    wip

--- airport-robot/airport_robot.go
+++ airport-robot/airport_robot.go
@@ -1,20 +1,15 @@
 package airportrobot
 
-type Greeter struct {
-    value int
-    // ...
+import "fmt"
+
+type Greeter interface {
+    LanguageName() string
+    Greet(name string) string
 }
 
-func (g Greeter) LanguageName() string {
-    l := string
-	return l
+func SayHello(name string, g Greeter) string {
+
 }
-func (g Greeter) Greet() string {
-    g := string
-	return g
-}
-// Write your code here.
-// This exercise does not have tests for each 
-// individual task.
-// Try to solve all the tasks first before running 
-// the tests.
+
+
+

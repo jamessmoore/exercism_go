24aaf6f89ab6af58c63bb6eeb99c1e35e3837021
James S. Moore <james@webtechhq.com>
2026-02-23 11:55:17 -0600

    wip

---
+++ airport-robot/airport_robot.go
@@ -0,0 +1,20 @@
+package airportrobot
+
+type Greeter struct {
+    value int
+    // ...
+}
+
+func (g Greeter) LanguageName() string {
+    l := string
+	return l
+}
+func (g Greeter) Greet() string {
+    g := string
+	return g
+}
+// Write your code here.
+// This exercise does not have tests for each 
+// individual task.
+// Try to solve all the tasks first before running 
+// the tests.

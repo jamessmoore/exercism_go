1c51297cef994cb0175cb76e69e736095db51f6b
James S. Moore <james@webtechhq.com>
2026-02-23 01:15:40 -0600

    wip

--- parsing-log-files/parsing_log_files.go
+++ parsing-log-files/parsing_log_files.go
@@ -9,7 +9,6 @@ func IsValidLine(text string) bool {
 	re := regexp.MustCompile(`^\[(TRC|DBG|INF|WRN|ERR|FTL)\]+\d*`)
 
 	if re.MatchString(text) {
-		// fmt.Println(text)
 		return true
 	}
 	return false
@@ -17,7 +16,6 @@ func IsValidLine(text string) bool {
 
 func SplitLogLine(text string) []string {
 	re := regexp.MustCompile(`\<([-*~=]*)\>`)
-	// fmt.Println(re.Split(text, -1))
 	return re.Split(text, -1) 
 }
 
@@ -29,39 +27,27 @@ func CountQuotedPasswords(lines []string) int {
 			ssub := re.FindStringSubmatch(line)
 			count += len(ssub)
 		}
-	// fmt.Println(count)
 	return count
 }
 
 func RemoveEndOfLineText(text string) string {
-	// re := regexp.MustCompile(`^.*end-of-line(?:\\d+.*)*$`)
 	re := regexp.MustCompile(`end-of-line\d*`)
-	// fmt.Println(text)
-	//.*end-of-line\d*.*
 	s := re.ReplaceAllString(text, "")
-	// fmt.Println(s)
 	return s
 }
 
 func TagWithUserName(lines []string) []string {
 	re := regexp.MustCompile(`User\s*[A-Z]\w*`)
-	var result []string
-	var newLine string
+	result := []string{}
 	for _, line := range lines {
 		fmt.Println(line)
-		if sm := re.FindStringSubmatch(line)
-			// for _, s := range sm {
-			// 	if s != "User" {
-			// 		fmt.Println(s)
-			// 	}
-			// }
-			// fmt.Printf("%q", sm)
-			// fmt.Printf("%q\n", re.FindStringSubmatch("-axxxbyc-"))
-			newLine = "[USR] " + line 
-		} 
-		newLine += line
-		// fmt.Println(newLine)
-		result = append(result, newLine)
+		sm := re.FindStringSubmatch(line)
+		
+		if sm != nil {
+			fmt.Println(sm)
+		// 	line = fmt.Sprintf("[USR] %s %s", sm[1], line)
+		}
+		result = append(result, line)
 	}
 	return result
 }

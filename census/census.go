6e5f3c800d2e2431fe49053ba9f89a291ee87ee8
James S. Moore <james@webtechhq.com>
2026-02-28 02:45:28 -0600

    census

--- census/census.go
+++ census/census.go
@@ -1,6 +1,8 @@
 // Package census simulates a system used to collect census data.
 package census
 
+import "fmt"
+
 // Resident represents a resident in this city.
 type Resident struct {
 	Name    string
@@ -11,7 +13,7 @@ type Resident struct {
 // NewResident registers a new resident in this city.
 func NewResident(name string, age int, address map[string]string) *Resident {
 	return &Resident {
-		Name: name, 
+		Name: name,  
 		Age: age,
 		Address: address,
 	}
@@ -19,7 +21,7 @@ func NewResident(name string, age int, address map[string]string) *Resident {
 
 // HasRequiredInfo determines if a given resident has all of the required information.
 func (r *Resident) HasRequiredInfo() bool {
-	// check for Resident
+	// check for nil Resident
 	if r == nil {
 		return false
 	}
@@ -27,14 +29,17 @@ func (r *Resident) HasRequiredInfo() bool {
 	if r.Name == "" {
 		return false
 	}
-
-	// check for street in address
+	
 	if r.Address == nil || len(r.Address) == 0 {
 		return false
 	}
 
+	// check for street in address
 	for k, v := range r.Address {
-		if k == "street" && v == "" {
+		if k != "street" {
+			return false
+		}
+		if v == "" {
 			return false
 		}
 
@@ -44,11 +49,20 @@ func (r *Resident) HasRequiredInfo() bool {
 
 // Delete deletes a resident's information.
 func (r *Resident) Delete() {
-	
+	fmt.Println(r)
+	r.Name = ""
+	r.Age = 0
+	r.Address = nil
+	//fmt.Println(r)
 }
 
 // Count counts all residents that have provided the required information.
 func Count(residents []*Resident) int {
 	var count int
+	for _, r := range residents {
+		if r.HasRequiredInfo() {
+			count ++
+		}
+	}
 	return count
 }
